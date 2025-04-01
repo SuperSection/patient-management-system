9326d926cdc43d8cafeb2c432d9960c2c2277c69
SuperSection <soumosarkar297@gmail.com>
2025-04-01 10:34:37 +0530

    Add Swagger integration and update API endpoints to include context-path

--- README.md
+++ README.md
@@ -17,3 +17,22 @@
 - Allow you to send or receive only the fields relevant to the client, rather than exposing all fields in your domain model.
 
 - Request DTOs allow you to validate client input (e.g., checking if fields are null or meet certain criteria).
+
+## Swagger Integration
+
+`springdoc-openapi` java library helps to automate the generation of API documentation using spring boot projects. It automatically generates documentation in **JSON/YAML and HTML format** APIs.
+
+1. Integrate swagger-ui, by adding the dependency in `pom.xml`
+
+    ```xml
+    <dependency>
+      <groupId>org.springdoc</groupId>
+      <artifactId>springdoc-openapi-starter-webmvc-ui</artifactId>
+      <version>2.8.6</version>
+   </dependency>
+    ```
+
+2. This will automatically deploy swagger-ui to a spring-boot application:
+
+    - The Swagger UI page will then be available at <http://server:port/context-path/swagger-ui.html>
+    - The OpenAPI description will be available at the following url for json format: <http://server:port/context-path/v3/api-docs>
--- patient-service/src/main/java/com/supersection/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/supersection/patientservice/controller/PatientController.java
@@ -19,11 +19,14 @@ import com.supersection.patientservice.dto.PatientResponseDTO;
 import com.supersection.patientservice.dto.validator.CreatePatientValidationGroup;
 import com.supersection.patientservice.service.PatientService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient", description = "API for managing Patients")
 public class PatientController {
   private final PatientService patientService;
 
@@ -32,12 +35,14 @@ public class PatientController {
   }
 
   @GetMapping
+  @Operation(summary = "Get Patients")
   public ResponseEntity<List<PatientResponseDTO>> getPatients() {
     List<PatientResponseDTO> patients = patientService.getPatients();
     return ResponseEntity.ok().body(patients);
   }
 
   @PostMapping
+  @Operation(summary = "Create a new Patient")
   public ResponseEntity<PatientResponseDTO> createPatient(
       @Validated({Default.class, CreatePatientValidationGroup.class})
       @RequestBody PatientRequestDTO patientRequestDTO
@@ -47,6 +52,7 @@ public class PatientController {
   }
 
   @PutMapping("/{id}")
+  @Operation(summary = "Update a Patient")
   public ResponseEntity<PatientResponseDTO> updatePatient(
       @PathVariable UUID id,
       @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO
@@ -56,6 +62,7 @@ public class PatientController {
   }
 
   @DeleteMapping("/{id}")
+  @Operation(summary = "Delete a Patient")
   public ResponseEntity<Void> deletePatient(@PathVariable UUID id) {
     patientService.deletePatient(id);
     return ResponseEntity.noContent().build();
