54e4e24664bf94c866c10dd3d36408bd5e9e5220
SuperSection <soumosarkar297@gmail.com>
2025-04-01 18:05:30 +0530

    Add Dockerfile for Patient Service with PostgreSQL container using docker-compose

--- patient-service/src/test/java/com/supersection/patientservice/PatientServiceApplicationTests.java
+++ patient-service/src/test/java/com/supersection/patientservice/PatientServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.supersection.patient_service;
+package com.supersection.patientservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
