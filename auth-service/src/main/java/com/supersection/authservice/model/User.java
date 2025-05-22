aa15956854d43ed00fc967d91add63cb24cf56c0
SuperSection <soumosarkar297@gmail.com>
2025-05-22 16:45:43 +0530

    Add initial auth-service implementation with Docker support, PostgreSQL configuration, and user model

---
+++ auth-service/src/main/java/com/supersection/authservice/model/User.java
@@ -0,0 +1,27 @@
+package com.supersection.authservice.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(unique = true, nullable = false)
+  private String email;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String role;
+}
