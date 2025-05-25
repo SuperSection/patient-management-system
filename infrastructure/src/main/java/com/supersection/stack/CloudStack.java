342a54921f4823d469a0d97c5fb4602d44303d04
SuperSection <soumosarkar297@gmail.com>
2025-05-26 01:36:36 +0530

    Refactor CloudStack to encapsulate VPC creation logic

--- infrastructure/src/main/java/com/supersection/stack/CloudStack.java
+++ infrastructure/src/main/java/com/supersection/stack/CloudStack.java
@@ -5,12 +5,27 @@ import software.amazon.awscdk.AppProps;
 import software.amazon.awscdk.BootstraplessSynthesizer;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+
 
 public class CloudStack extends Stack {
 
+  private final Vpc vpc;
+
   public CloudStack(final App scope, final String id, final StackProps props) {
     super(scope, id, props);
+
+    this.vpc = createVpc();
   }
+
+
+  private Vpc createVpc() {
+    return Vpc.Builder.create(this, "PatientManagmentVPC")
+        .vpcName("PatientManagmentVPC")
+        .maxAzs(3) // Default is all AZs in the region
+        .build();
+  }
+
 
   public static void main(final String[] args) {
     App app = new App(AppProps.builder().outdir("./cdk.out").build());
